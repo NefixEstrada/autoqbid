9565c003e3048f5ac716c4d139ac82bdad4dbc02
Néfix Estrada <nefixestrada@gmail.com>
2018-05-16 18:09:46 +0200

    Created the class structure
    The class is empty but with all the required functions

---
+++ autoqbid.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class AutoQbid:
+    """
+    Main class for the program
+    """
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def login(self):
+        pass
+
+    def move_to_month(self):
+        pass
+
+    def open_day_form(self):
+        pass
+
+    def fill_activity_log(self):
+        pass
+
+    def fill_day(self):
+        pass
