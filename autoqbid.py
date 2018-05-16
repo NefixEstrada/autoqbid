02525793495abb4c5322f00964bd276c0510a91a
Néfix Estrada <nefixestrada@gmail.com>
2018-05-16 18:34:23 +0200

    Finished with the __init__ function
    You can create a qbid instance now. Using your favourite browser :D

--- autoqbid.py
+++ autoqbid.py
@@ -1,13 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Imports
+from selenium import webdriver
+
 
 class AutoQbid:
     """
     Main class for the program
     """
-    def __init__(self):
-        pass
+    def __init__(self, username, password, year=None, month=None, day=None, form_data=[], browser="Firefox"):
+        self.username = username
+        self.password = password
+
+        self.date = dict()
+        if year:
+            self.date["year"] = year
+
+        if month:
+            self.date["month"] = month
+
+        if day:
+            self.date["day"] = day
+
+        self.form_data = [{
+            "activity_id": activity[0],
+            "hours": activity[1]
+        } for activity in form_data]
+
+        try:
+            self.driver = getattr(webdriver, browser)()
+
+            try:
+                self.driver.maximize_window()
+
+            except Exception:
+                print("Sorry, your browser doesn't support maximizing the window!")
+
+            self.driver.get("https://www.empresaiformacio.org/sBidAlumne/")
+            self.driver.switch_to.frame(1)
+
+        except Exception:
+            pass
 
     def __del__(self):
         pass
